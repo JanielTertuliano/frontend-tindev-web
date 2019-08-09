f0380b499b46e4f40f00341cd4bd8b6abdab1277
Janiel <janiel.web@gmail.com>
2019-08-09 12:22:02 -0300

    Add socket-client; add Match

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css'
 import logo from '../../assets/logo.svg';
 import like from '../../assets/like.svg';
 import dislike from '../../assets/dislike.svg';
+import itsamatch from '../../assets/itsamatch.png';
+
 import api from '../../services/api';
+
 
 export default function Main({ match }) {
 
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(false);
 
     useEffect(() => {
         async function loadUsers() {
@@ -20,6 +25,17 @@ export default function Main({ match }) {
         }
 
         loadUsers();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3000', {
+            query: { user: match.params.id}
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
+
     }, [match.params.id]);
 
     async function handleLike(id) {
@@ -58,7 +74,7 @@ export default function Main({ match }) {
                                     <img src={dislike} alt="Dislike" />
                                 </button>
                                 <button type="button" onClick={() => handleLike(user._id)} >
-                                    <img src={like} alt="Like"/>
+                                    <img src={like} alt="Like" />
                                 </button>
                             </div>
                         </li>
@@ -68,6 +84,17 @@ export default function Main({ match }) {
             ) : (
                     <div className="empty" >Users Empty</div>
                 )}
+
+                { matchDev && (
+                    <div className="match-container">
+                        <img src={itsamatch}/>
+                        <img className="avatar" src={matchDev.avatar}/>
+                        <strong>{matchDev.name}</strong>
+                        <p>{matchDev.bio}</p>
+
+                        <button type="button" onClick={() => setMatchDev(null)}>Fechar</button>
+                    </div>
+                ) }
         </div>
     );
 }
